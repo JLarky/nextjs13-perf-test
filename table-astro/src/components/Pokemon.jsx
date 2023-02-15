9f673680a06fb669aba89ab3f9b6f49b4c4a5bf5
JLarky <jlarky@gmail.com>
2023-02-15 01:08:06 -0700

    astro 46kb page, 48kb bundle but index.html is 464K 🙃 instead of 600K

--- table-astro/src/components/Pokemon.jsx
+++ table-astro/src/components/Pokemon.jsx
@@ -2,13 +2,11 @@ const pokemon =
   typeof document !== "undefined" &&
   JSON.parse(window.__ASTRO_DATA__.textContent).pokemon;
 
-export default function Pokemon({ name, children }) {
+export default function Pokemon({ id, children }) {
   return (
     <a
       href="#"
-      onClick={() =>
-        alert(JSON.stringify(pokemon.find((p) => p.name === name)))
-      }
+      onClick={() => alert(JSON.stringify(pokemon.find((p) => p.id === id)))}
     >
       {children}
     </a>
