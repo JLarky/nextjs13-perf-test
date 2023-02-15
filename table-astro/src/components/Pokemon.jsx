5f06d9156b03b108039653acfb8e6729f2729da2
JLarky <jlarky@gmail.com>
2023-02-15 01:02:37 -0700

    astro 46kb page, 48kb bundle
    does that matter? 480K instead of 600K

--- table-astro/src/components/Pokemon.jsx
+++ table-astro/src/components/Pokemon.jsx
@@ -1,6 +1,15 @@
-export default function Pokemon({ data, children }) {
+const pokemon =
+  typeof document !== "undefined" &&
+  JSON.parse(window.__ASTRO_DATA__.textContent).pokemon;
+
+export default function Pokemon({ name, children }) {
   return (
-    <a href="#" onClick={() => alert(JSON.stringify(data))}>
+    <a
+      href="#"
+      onClick={() =>
+        alert(JSON.stringify(pokemon.find((p) => p.name === name)))
+      }
+    >
       {children}
     </a>
   );
